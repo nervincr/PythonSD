f469b6e8ce6f6e315c8602a458e3c1a519d34b15
nervincr <nervincr@hotmail.com>
2019-09-17 21:38:53 -0600

    Final

--- Lab2RelojesVec.py
+++ Lab2RelojesVec.py
@@ -11,20 +11,27 @@ def server(proceso, arreglo, host, puerto):
     while True:
         (c, addr) = s.accept()
 
-        recv = c.recv(1024).decode("utf-8")
+        recv = c.recv(1024).decode("utf-8").split(',')
 
-        arreglo[int(recv.split(',')[0])] = int(recv.split(',')[1])
+        if arreglo[0] < int(recv[0]):
+            arreglo[0] = int(recv[0])
+        if arreglo[1] < int(recv[1]):
+            arreglo[1] = int(recv[1])
+        if arreglo[2] < int(recv[2]):
+            arreglo[2] = int(recv[2])
+
         arreglo[proceso] = arreglo[proceso] + 1
 
         c.close()
 
-        logging.critical(','.join(str(e) for e in arreglo))
+        logging.critical(",".join(str(e) for e in arreglo))
         f=open("log.txt","a+")
-        f.write(','.join(str(e) for e in arreglo))
+        f.write(",".join(str(e) for e in arreglo) + "\n")
         f.close()
 
 if __name__ == "__main__":
-    ipsprocesos = ["172.17.117.131", "172.17.117.131", "172.17.117.131"]
+    ipsprocesos = ["172.17.117.131", "172.17.117.148", "172.17.117.145"]
+    # ipsprocesos = ["172.17.117.131", "172.17.117.131", "172.17.117.131"]
     puertosprocesos = [8081, 8082, 8083]
     arreglo = [0, 0, 0]
     
@@ -42,11 +49,11 @@ if __name__ == "__main__":
 
         c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         c.connect((ipsprocesos[enviar], puertosprocesos[enviar]))
-        c.send("{},{}".format(proceso, arreglo[proceso]).encode("utf-8"))
+        c.send(",".join(str(e) for e in arreglo).encode("utf-8"))
         c.close()
 
-        logging.critical(','.join(str(e) for e in arreglo))
+        logging.critical(",".join(str(e) for e in arreglo))
         f=open("log.txt","a+")
-        f.write(','.join(str(e) for e in arreglo))
+        f.write(",".join(str(e) for e in arreglo) + "\n")
         f.close()
     
